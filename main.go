fb5bbb5bc1270d3d6f8a89b7ba0676fda8ab5c43
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-06-11 13:20:09 +0600

    Refactor variable declaration in main function and update print statement

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	//* Varibales
+	// var name string = "Infan"
+	// name := "Infan"
+	//! name string := "Mizan" Wrong Way
+	// var name = "infan"
+	//* Group Variable
+	var (
+		name string = "Infan"
+		age  int    = 22
+	)
+	fmt.Println(name)
+	fmt.Println(age)
 }
