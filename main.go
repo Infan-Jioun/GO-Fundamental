553a58922f3524b0140c746c655be0b42e9fa8ad
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-06-13 11:46:03 +0600

    Remove commented variable declarations and clean up print statements in main function

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+//* Function
+func makeCoffee() {
+	fmt.Println("Making Coffee .....")
+}
 func main() {
 	//* Varibales
 	// var name string = "Infan"
@@ -9,16 +13,28 @@ func main() {
 	//! name string := "Mizan" Wrong Way
 	// var name = "infan"
 	//* Group Variable
-	var (
-		name string = "Infan"
-		age  int    = 22
-	)
+	// var (
+	// 	name string = "Infan"
+	// 	age  int    = 22
+	// )
 	//* Multiple Varibales declaration
 	// var x, y int
 	// x = 35
 	// y = 20
-	const x, y string = "Infan", "Jioun"
-	fmt.Println(name)
-	fmt.Println(age)
-	fmt.Println(x, y)
+	// const x, y string = "Infan", "Jioun"
+	// fmt.Println(name)
+	// fmt.Println(age)
+	// fmt.Println(x, y)
+	// * Zero Variable
+	// var age int //* 0
+	// fmt.Println((age))
+	// var name string //* ""
+	// fmt.Println(name)
+	// var isAdmin bool
+	// fmt.Println(isAdmin) //* false
+	// var score float64    //* 0
+	// fmt.Println(score)
+	// *  Function Call
+	makeCoffee()
+
 }
